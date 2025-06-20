9a26e019ed8386d26ebf480bf96705b472b1dc9b
nephi@blaviken-wsl <nephi.norton@gmail.com>
2025-06-19 23:51:39 -0600

    refactored commands; changed database

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::time::Duration;
 
-use ::serenity::all::Permissions;
 use ::serenity::all::{
     ActivityData, CacheHttp, Context as SerenityCtx, CreateCommand, CreateCommandOption, FullEvent,
     Interaction, RoleId, UserId,
 };
+use ::serenity::all::{GuildId, Permissions};
 use dotenv::dotenv;
 use humantime::format_duration;
 use humantime::parse_duration;
@@ -13,7 +13,10 @@ use setup_commands::*;
 use sqlx::postgres::types::PgInterval;
 use sqlx::{PgPool, postgres::PgPoolOptions};
 
+use stefan_traits::*;
+
 mod setup_commands;
+mod stefan_traits;
 
 // User data, which is stored and accessible in all command invocations
 pub(crate) struct Data {
@@ -22,34 +25,6 @@ pub(crate) struct Data {
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
-
-// https://github.com/coravacav/uofu-cs-discord-bot/blob/0513983163f0563a26709004dccb954948dffb2a/bot-lib/src/utils.rs
-pub trait GetRelativeTimestamp {
-    fn discord_relative_timestamp(&self) -> String;
-}
-
-impl GetRelativeTimestamp for chrono::DateTime<chrono::Utc> {
-    fn discord_relative_timestamp(&self) -> String {
-        format!("<t:{}:R>", self.timestamp())
-    }
-}
-
-trait SendReplyEphemeral {
-    async fn reply_ephemeral(&self, content: impl Into<String>) -> Result<(), Error>;
-}
-
-impl SendReplyEphemeral for Context<'_> {
-    async fn reply_ephemeral(&self, content: impl Into<String>) -> Result<(), Error> {
-        let reply = CreateReply::default()
-            .reply(true)
-            .ephemeral(true)
-            .content(content);
-
-        self.send(reply).await?;
-
-        Ok(())
-    }
-}
 
 fn get_formatted_message(
     message: &String,
@@ -68,7 +43,7 @@ fn get_formatted_message(
 /// Tells you the total time kenneled in case you can't read the status
 #[poise::command(slash_command)]
 async fn time_kenneled(ctx: Context<'_>) -> Result<(), Error> {
-    let pool = &ctx.data().pool;
+    let Data { pool } = ctx.data();
 
     match sqlx::query!(
         r#"
@@ -110,6 +85,8 @@ async fn kennel_user(
     #[description = "User to kennel"] user: UserId,
     #[description = "Time to kennel"] time: String,
 ) -> Result<(), Error> {
+    let guild_id = ctx.guild_id().expect("If this command gets called outside of a guild somehow, the world is on fire, and everyone explodes.");
+
     let Ok(dur_time) = parse_duration(&time) else {
         return ctx
             .reply_ephemeral("Invalid time format! Say something like '3m' or '1h'")
@@ -120,59 +97,64 @@ async fn kennel_user(
         return ctx.reply_ephemeral("Over 1 second, please...").await;
     }
 
-    let data = sqlx::query!(
+    let Ok(data) = sqlx::query!(
         r#"
         SELECT * FROM servers
         WHERE
-        guild_id = $1
-        AND command_name = $2
-        ;
+            guild_id = $1
+            AND command_name = $2
+            ;
         "#,
-        format!("{}", ctx.guild_id().ok_or("No guild ID!")?.get()),
+        guild_id.get().to_string(),
         ctx.invoked_command_name()
     )
     .fetch_one(&ctx.data().pool)
-    .await?;
+    .await
+    else {
+        ctx.reply_ephemeral("Set kennel role first!").await?;
 
-    let Some(role_id) = data.role_id else {
-        return ctx.reply_ephemeral("Set kennel role first!").await;
+        return Ok(());
     };
-    let http = ctx.http();
-    let guild = ctx.guild_id().expect("No guild ID!");
-    let member = guild.member(http, user).await?;
-    let role_id = RoleId::new(role_id.parse::<u64>()?);
-    let return_time = chrono::Utc::now() + dur_time.clone();
 
+    let http = ctx.http();
+    let member = guild_id.member(http, user).await?;
+    let role_id = RoleId::new(
+        data.role_id
+            .parse::<u64>()
+            .expect("Invalid role_id data inserted into database! WTF?"),
+    );
+    let author_id = ctx.author().id;
+    let return_time = chrono::Utc::now() + dur_time.clone();
     let reply = get_formatted_message(
-        &data.command_verb.unwrap_or(
-            "$kenneler has kenneled $victim for $time.\n\nThey will be released $return."
-                .to_string(),
-        ),
+        &data.command_verb,
         &user,
         &ctx.author().id,
         &time,
         &return_time.discord_relative_timestamp(),
     );
+    let pg_int = PgInterval::try_from(dur_time).expect("Some fuckwit put in a microsecond value?");
 
     member.add_role(http, &role_id).await?;
 
     let reply_handle = ctx.reply(reply).await?;
 
-    if let Ok(pg_int) = PgInterval::try_from(dur_time) {
-        sqlx::query!(
-            r#"
-            INSERT INTO kennelings(guild_id, user_id, kennel_length, kenneled_at)
-            VALUES($1, $2, $3, NOW())
+    sqlx::query!(
+        r#"
+        INSERT INTO kennelings
+            (guild_id, victim, kenneler, kennel_length)
+        VALUES
+            ($1, $2, $3, $4)
             ;
         "#,
-            format!("{}", guild.get()),
-            format!("{}", user.get()),
-            pg_int,
-        )
-        .execute(&ctx.data().pool)
-        .await?;
-    }
+        guild_id.get().to_string(),
+        user.get().to_string(),
+        author_id.get().to_string(),
+        pg_int,
+    )
+    .execute(&ctx.data().pool)
+    .await?;
 
+    // TODO: Turn this into a helper function
     if let Ok(res) = sqlx::query!(
         r#"
         SELECT SUM(kennel_length)
@@ -206,11 +188,9 @@ async fn kennel_user(
         .edit(
             ctx,
             CreateReply::default().content(get_formatted_message(
-                &data
-                    .release_message
-                    .unwrap_or("$victim has been released from the kennel.".to_string()),
+                &data.release_message,
                 &user,
-                &ctx.author().id,
+                &author_id,
                 &time,
                 &return_time.discord_relative_timestamp(),
             )),
@@ -228,8 +208,12 @@ async fn wildcard_command_handler(
 ) -> Result<(), Error> {
     if let FullEvent::InteractionCreate { interaction } = event {
         if let Interaction::Command(command_interaction) = interaction {
+            println!("{}", command_interaction.data.name);
+            // TODO: Is there a better way to check this?
             if command_interaction.data.name == "set_kennel_command"
                 || command_interaction.data.name == "set_kennel_role"
+                || command_interaction.data.name == "set_kennel_message"
+                || command_interaction.data.name == "set_release_message"
             {
                 return Ok(());
             }
@@ -277,6 +261,7 @@ async fn main() {
                 set_kennel_role(),
                 set_kennel_command(),
                 set_kennel_message(),
+                set_release_message(),
                 time_kenneled(),
             ],
             event_handler: |w, x, y, z| Box::pin(wildcard_command_handler(w, x, y, z)),
@@ -286,56 +271,38 @@ async fn main() {
             Box::pin(async move {
                 let pool = PgPoolOptions::new()
                     .max_connections(5)
-                    .connect(postgres_url.as_str())
+                    .connect(&postgres_url)
                     .await?;
 
                 let data = sqlx::query!(
                     r#"
-                SELECT * FROM servers
-                ;
-                "#
+                    SELECT * FROM servers
+                    ;
+                    "#
                 )
                 .fetch_all(&pool)
                 .await?;
 
-                for thing in data {
-                    if let Some(name) = thing.command_name {
-                        let cmd = CreateCommand::new(&name)
-                            .description("Punish a user!")
-                            .add_option(
-                                CreateCommandOption::new(
-                                    serenity::CommandOptionType::User,
-                                    "user",
-                                    "User to be punished",
-                                )
-                                .required(true),
-                            )
-                            .add_option(
-                                CreateCommandOption::new(
-                                    serenity::CommandOptionType::String,
-                                    "time",
-                                    "How long to punish the user",
-                                )
-                                .required(true),
-                            )
-                            .default_member_permissions(Permissions::MODERATE_MEMBERS);
+                for row in data {
+                    let cmd = get_kennel_command_struct(&row.command_name);
 
-                        ctx.http()
-                            .create_guild_commands(
-                                thing.guild_id.parse::<u64>()?.into(),
-                                &vec![cmd],
-                            )
-                            .await?;
-                    }
+                    let guild_id = row
+                        .guild_id
+                        .parse::<u64>()
+                        .expect("Malformed guild_id data was inserted into the database! WTF?");
+
+                    ctx.http()
+                        .create_guild_commands(guild_id.into(), &vec![cmd])
+                        .await?;
                 }
 
                 if let Ok(res) = sqlx::query!(
                     r#"
-                        SELECT SUM(kennel_length)
-                        FROM kennelings
-                        WHERE 
-                            NOT guild_id = '849505364764524565'
-                            ;
+                    SELECT SUM(kennel_length)
+                    FROM kennelings
+                    WHERE 
+                        NOT guild_id = '849505364764524565'
+                        ;
                     "#
                 )
                 .fetch_one(&pool)
