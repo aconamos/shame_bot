c722f4853b4fc5d0bf53629b5156e5d2af2357b3
nephi@blaviken-wsl <nephi.norton@gmail.com>
2025-07-30 18:38:41 -0600

    schema update (just the rename)

--- schema.sql
+++ schema.sql
@@ -1,13 +1,18 @@
 -- Adminer 5.3.0 PostgreSQL 17.5 dump
 
 DROP TABLE IF EXISTS "kennelings";
+DROP SEQUENCE IF EXISTS kennelings_id_seq;
+CREATE SEQUENCE kennelings_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
 CREATE TABLE "public"."kennelings" (
     "guild_id" character varying(128) NOT NULL,
-    "victim" character varying(128) NOT NULL,
+    "victim_id" character varying(128) NOT NULL,
     "kennel_length" interval NOT NULL,
     "kenneled_at" timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    "kenneler" character varying(128) NOT NULL,
-    "released_at" timestamp GENERATED ALWAYS AS ((kenneled_at + kennel_length)) STORED NOT NULL
+    "author_id" character varying(128) NOT NULL,
+    "released_at" timestamp GENERATED ALWAYS AS ((kenneled_at + kennel_length)) STORED NOT NULL,
+    "id" integer DEFAULT nextval('kennelings_id_seq') NOT NULL,
+    CONSTRAINT "kennelings_pkey" PRIMARY KEY ("id")
 )
 WITH (oids = false);
 
@@ -30,4 +35,4 @@ WITH (oids = false);
 CREATE INDEX servers_guild_id_command_name ON public.servers USING btree (guild_id, command_name);
 
 
--- 2025-07-24 20:38:01 UTC
+-- 2025-07-31 00:38:25 UTC
